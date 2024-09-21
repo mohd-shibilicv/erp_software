de5b8fd456347c28f8e12996d598906069503ea5
mohd-shibilicv <project.nasra@gmail.com>
2024-09-21 09:57:31 +0300

    change: sqlite3 as development database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -99,24 +99,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'erp',
-        'USER': 'postgres',
-        'PASSWORD':'1111',
-        'HOST':'localhost',
-        'PORT': '',
-        
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'erp',
+            'USER': 'postgres',
+            'PASSWORD':'1111',
+            'HOST':'localhost',
+            'PORT': '',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
