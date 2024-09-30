27062f944ebffaced36d80d66e8e15ecba4b73b7
nashir <nashirnoor1718@gmail.com>
2024-09-30 13:44:16 +0530

    projects updates

--- frontend/src/pages/storePages/Projects/addProject.jsx
+++ frontend/src/pages/storePages/Projects/addProject.jsx
@@ -17,7 +17,6 @@ import { useState } from "react";
 import { v4 as uuid } from "uuid";
 import { api } from "@/services/api";
 import { clientRequirementService } from "@/services/crmServiceApi";
-import { clientQuotation } from "@/services/crmServiceApi";
 
 export default function AddnewProject() {
   const [projectId, setProjectid] = useState("");
@@ -38,12 +37,12 @@ export default function AddnewProject() {
     try {
       setSelectLoading(true);
       const { data } = await api.get('/clients/');
-      const response = await clientQuotation.getAll();
+      const response = await clientAgreement.getAll();
       const req = await clientRequirementService.getAll();
       const staff = await api.get("/staff/")
       console.log(staff.data,"This is staff")
       console.log(req.data.results,"requirement")
-      console.log(response.data.results,"Quotation")
+      console.log(response.data.results,"Agreement")
       console.log(data.results,"Client")
 
       setClient(data.results);
